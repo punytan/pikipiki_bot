19884ea5fce7fa16ef5e3dfb4a3fb263a2c75d8c
punytan <punytan@gmail.com>
2013-01-20 15:22:44 +0900

    remove configs from repository

--- config/piki/words.pl
+++
@@ -1,8 +0,0 @@
-use utf8;
-[
-    'C言語', 'C\+\+', 'C#', 'F#', 'Objective-C', 'COBOL',
-    'D言語', 'Delphi', 'FORTRAN', 'Groovy', 'JavaScript', 'Java', 'Pascal', 'Perl', 'Python',
-    'Ruby', '機械語', 'アセンブリ', 'アセンブラ', 'Erlang', 'Haskell', 'LISP', 'OCaml', 'Scala',
-    'ActionScript', 'Smalltalk', 'プログラミング', 'プログラマ',
-]; # Regexp. Except PHP, SQL and AWK
-
